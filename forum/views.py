e62ab6c52e7b0d620c2394a39212941d08aadc93
Muhammad Akbari <86768786+mili91@users.noreply.github.com>
2021-12-22 00:58:59 +0330

    feat (forum): create and set the views and urls files

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'forum/index.html')
+
+def thread(request):
+    return render(request, 'forum/thread.html')
