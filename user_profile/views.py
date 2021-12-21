abfbf135f5559549e8b4f0e958c4cef0dcd0012b
Muhammad Akbari <86768786+mili91@users.noreply.github.com>
2021-12-22 01:28:45 +0330

    feat (user_profile): create and set the views and urls files

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def register(request):
+    return render(request, 'user_profile/register.html')
+
+def profile(request):
+    return render(request, 'user_profile/profile.html')
