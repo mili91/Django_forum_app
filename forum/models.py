baf2bab100e4b2c505ab20c943310fc19815c885
Muhammad Akbari <86768786+mili91@users.noreply.github.com>
2021-12-27 18:48:40 +0330

    feat (forum_model): add thread and reply models and mnigrate them

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Thread(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    date_created = models.DateField(auto_now_add=True)
 
-# Create your models here.
+class Reply(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
