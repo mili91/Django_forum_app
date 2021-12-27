409b1ab5dceb92c965df6fff4c24b80fc51ce8f6
Muhammad Akbari <86768786+mili91@users.noreply.github.com>
2021-12-27 12:47:25 +0330

    feat (user_profile_views): enable register-page form and add its backend

---
+++ user_profile/forms.py
@@ -0,0 +1,27 @@
+from typing import Any
+from django import forms
+from django.contrib.auth.models import User 
+from django.contrib.auth.forms import UserCreationForm
+
+
+class ExtendedUserCreationForm (UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class' : 'form-control', 'placeholder' : 'Enter a username', 'tabindex' : '1'})
+        self.fields['email'].widget.attrs.update({'class' : 'form-control', 'placeholder' : 'Enter email', 'tabindex' : '2'})
+        self.fields['password1'].widget.attrs.update({'class' : 'form-control', 'placeholder' : 'Enter a password', 'tabindex' : '3'})
+        self.fields['password2'].widget.attrs.update({'class' : 'form-control', 'placeholder' : 'confirm', 'tabindex' : '4'})
+        
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+        
+        return user
--- user_profile/views.py
+++ user_profile/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from .forms import ExtendedUserCreationForm
+
 
 def register(request):
-    return render(request, 'user_profile/register.html')
+    if request.method == 'POST':
+        form = ExtendedUserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            return redirect('index')
+        
+    else:
+        form = ExtendedUserCreationForm()
+
+    context = {'form' : form}
+    return render(request, 'user_profile/register.html', context)
 
 def profile(request):
     return render(request, 'user_profile/profile.html')
