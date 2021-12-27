15c66236b5b0f32caaf99583dba2f2f3d0a99909
Muhammad Akbari <86768786+mili91@users.noreply.github.com>
2021-12-27 23:32:46 +0330

    feat (user_profile-views): add logout url and its redirect url

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='user_profile/login.html', authentication_form=ExtendedAuthenticationForm)),
+    path('logout/', auth_views.LogoutView.as_view()),
 ]
